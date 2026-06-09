df39aaf29c13646c58085d66439fa5961f6537c0
LeonardoBrambilla <leonardovbalves@gmail.com>
2026-06-09 09:08:45 -0300

    feat: add form structure and style

---
+++ src/app/components/forms/forms.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  imports: [],
+  templateUrl: './forms.html',
+  styleUrl: './forms.scss',
+})
+export class Forms {
+  results = [];
+
+}
